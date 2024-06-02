5a3fc0964d92bf1b089a7adedd1bb2d41e5c00e8
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-06-02 00:21:23 -0600

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -79,7 +79,8 @@ Help:
 
                file_name = file_name.replace("'", "", 2)
                path = path_directory + file_name
-
+               delete_secure(path)
+                     
             else:
                 path= path_directory + file_name
                 delete_secure(path)
