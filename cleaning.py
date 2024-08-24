3bc6510b27001219fcc8bb92a7f0f49ae33d3f4a
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-23 19:58:51 -0600

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -4,21 +4,21 @@ from cryptography.fernet import Fernet
 from sys import argv
 from os import remove, urandom, path
 
-def delete_secure(path):
+def delete_secure(path_local):
           overwrite = urandom(512)
           for _ in range(2):
               key = Fernet.generate_key()
               fernet = Fernet(key)
               key = overwrite
               del(key)
-              with open(path,'rb') as read:
+              with open(path_local,'rb') as read:
                 file_read=read.read()
               encryption = fernet.encrypt(file_read)
               fernet = overwrite
               del(fernet)
-              with open(path,'wb') as write:
+              with open(path_local,'wb') as write:
                 write.write(encryption)
-          remove(path)
+          remove(path_local)
           print("safely deleted file!")
           return
           
@@ -42,12 +42,12 @@ def main():
             file_name = input("Enter file name: ")
             if "'" in file_name:
                 file_name = file_name.replace("'", "")
-                path = path.join(path_directory, file_name)
-                delete_secure(path)
+                path_local = path.join(path_directory, file_name)
+                delete_secure(path_local)
                     
             else:
-                path= path.join(path_directory, file_name)
-                delete_secure(path)
+                path_local = path.join(path_directory, file_name)
+                delete_secure(path_local)
 
 
  except (KeyboardInterrupt,EOFError):
