e0dcf945477464c8e33bb2b7a642c8adef63869f
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-23 20:31:26 -0600

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -36,7 +36,6 @@ def main():
  try:
    if "-h" in argv or "--help" in argv:
             show_help()
-         
    elif "-f" in argv:
             path_directory = input("Enter the path where your file is: ")
             file_name = input("Enter file name: ")
@@ -44,25 +43,21 @@ def main():
                 file_name = file_name.replace("'", "")
                 path_local = path.join(path_directory, file_name)
                 delete_secure(path_local)
-                    
             else:
                 path_local = path.join(path_directory, file_name)
                 delete_secure(path_local)
 
-
  except (KeyboardInterrupt,EOFError):
           print("\nOperation canceled by user!")
-          
  except FileNotFoundError:
           print("Non-existent route!")
-          
  except PermissionError:
           print("You do not have permissions on that file!")
-          
-
+ except IsADirectoryError:        
+          print("Please enter a valid file name!")
+           
 if __name__ == "__main__":
        main()
-
 
 __name__="cleaning"
 __version__="1.0"
