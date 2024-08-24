5e4d2bf5733d2bef706b4e48e775e8feaa524690
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-24 09:42:43 -0600

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -5,68 +5,66 @@ from sys import argv
 from os import remove, urandom, path
 
 def delete_secure(path_local):
-          overwrite = urandom(512)
-          for _ in range(2):
-              key = Fernet.generate_key()
-              fernet = Fernet(key)
-              key = overwrite
-              del(key)
-              with open(path_local,'rb') as read_local:
-                file_read=read_local.read()
-              encryption = fernet.encrypt(file_read)
-              fernet = overwrite
-              del(fernet)
-              with open(path_local,'wb') as write_local:
-                write_local.write(encryption)
-          remove(path_local)
-          print("safely deleted file!")
-          return
+    overwrite = urandom(512)
+    for _ in range(2):
+        key = Fernet.generate_key()
+        fernet = Fernet(key)
+        key = overwrite
+        del(key)
+        with open(path_local,'rb') as read_local:
+            file_read=read_local.read()
+        encryption = fernet.encrypt(file_read)
+        fernet = overwrite
+        del(fernet)
+        with open(path_local,'wb') as write_local:
+            write_local.write(encryption)
+    remove(path_local)
+    print("safely deleted file!")
+    return
           
 def delete_lite(path_local):
-          #method not recommended because it does not guarantee complete overwriting 
-          overwrite = urandom(2048)
-          with open(path_local,'wb') as lite:
-             for _ in range(1000):
-                    lite.write(overwrite)
-             lite.truncate(2048 * 500)
-          remove(path_local)
-          print("File overwritten and deleted")
+    #method not recommended because it does not guarantee complete overwriting 
+    overwrite = urandom(2048)
+    with open(path_local,'wb') as lite:
+        for _ in range(1000):
+            lite.write(overwrite)
+        lite.truncate(2048 * 500)
+    remove(path_local)
+    print("File overwritten and deleted")
 
 
 def show_help():
-          print("""
+    print("""
 Cleaning: is a tool that allows you to safely delete multimedia files.
 Usage:
     python3 cleaning.py -s  option that allows me to securely delete multimedia files (RECOMMENDED)
     python3 cleaning.py -l  option only when the multimedia file is very large and from a smartphone
     python3 cleaning.py [-h,--help]  print help menu
-                 """)
+          """)
 
+
+def data_entry():                                                                                 
+   path_directory = input("Enter the path where your file is: ")                         
+   file_name = input("Enter file name: ")
+   if "'" in file_name:
+       file_name = file_name.replace("'", "")
+        path_local = path.join(path_directory, file_name)
+        return path_local
+
+   else:
+        path_local = path.join(path_directory, file_name)
+        return path_local
 
 def main():
  try:
    if "-h" in argv or "--help" in argv:
             show_help()
    elif "-s" in argv:
-            path_directory = input("Enter the path where your file is: ")
-            file_name = input("Enter file name: ")
-            if "'" in file_name:
-                file_name = file_name.replace("'", "")
-                path_local = path.join(path_directory, file_name)
-                delete_secure(path_local)
-            else:
-                path_local = path.join(path_directory, file_name)
-                delete_secure(path_local)
+            path_local = data_entry()
+            delete_secure(path_local)
    elif "-l" in argv:
-            path_directory = input("Enter the path where your file is: ")
-            file_name = input("Enter file name: ")
-            if "'" in file_name:
-                file_name = file_name.replace("'", "")
-                path_local = path.join(path_directory, file_name)
-                delete_lite(path_local)
-            else:
-                path_local = path.join(path_directory, file_name)
-                delete_lite(path_local)
+            path_local = data_entry()
+            delete_lite(path_local)
                       
  except (KeyboardInterrupt,EOFError):
           print("\nOperation canceled by user!")
