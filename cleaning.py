74add8bf11ab02fe951b5e8c10405c6eb81aec87
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-08-26 19:29:20 -0600

    cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -5,6 +5,28 @@ from os import remove, urandom, path
         
 def delete_secure(path_local):
     size = path.getsize(path_local)
+    sizes_kb = size / 1024
+    sizes_mb = sizes_kb / 1024
+    if size >= 0 and sizes_kb <= 1024:
+         salt = 1000
+    elif sizes_mb >=1  and sizes_mb <= 235:
+          salt = 120000
+    elif sizes_mb >= 235 and sizes_mb <= 587:
+          salt = 300000
+    elif  sizes_mb >=587  and sizes_mb <= 978:
+          salt = 500000
+    elif sizes_mb >= 978 and sizes_mb <= 2000:
+          salt = 1000000
+    elif sizes_mb >= 2000 and sizes_mb <= 4400:
+          salt = 2300000
+    elif sizes_mb >= 4400 and sizes_mb <= 6200:
+          salt = 3200000
+    elif sizes_mb >= 6200 and sizes_mb <= 9600:
+          salt = 5000000
+    elif sizes_mb >= 9600 and sizes_mb <= 12000:
+          salt = 6200000
+    else:
+         print("Has exceeded me you have exceeded the maximum allowed length which is 12G ")
     with open(path_local,'wb') as lite:
         for _ in range(salt):
            overwrite = urandom(2048)
